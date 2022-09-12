3e2d6afb7ac5893ef4546c8994267e8620ff691a
Olajide Oluwatosin <oluwatosinaina424@gmail.com>
2022-09-12 12:14:59 +0100

    Add file for testing

--- tests/test_train.py
+++ tests/test_train.py
@@ -8,19 +8,15 @@ from pipeline.train import get_data, prepare_data
 warnings.filterwarnings("ignore")
 
 
-def test_prepare_data():
+def test_get_prepare_data():
     """
     Test the prepare_data function for the continuous_training
     """
-    # data = pd.read_csv("data/realtor-data.csv")
     data = get_data.fn()
 
     features, labels = prepare_data.fn(data, None)
 
     expected_features = pd.read_csv("data/processed_features.csv")
-
-    # pd.testing.assert_series_equal(labels, expected_labels)
-    # pd.testing.assert_frame_equal(features, expected_features)
 
     assert features.columns.to_list() == expected_features.columns.to_list()
     assert not features.isna().any().any()
