477f97662d2e2b00ac396151800654d97117fde7
Olajide Oluwatosin <oluwatosinaina424@gmail.com>
2022-09-12 12:54:47 +0100

    Fix directory bug with integration test

--- integration_test/integration.sh
+++ integration_test/integration.sh
@@ -2,23 +2,20 @@
 
 export S3_ENDPOINT="http://localhost:4566"
 
-if [ -z "${GITHUB_ACTIONS}" ]; then
-    cd "$(dirname "$0")"
-fi
-
 if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
     export LOCAL_IMAGE_NAME=mlops-capstone:latest
     echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
     aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 123456789012.dkr.ecr.us-east-1.amazonaws.com
-    cd ../src
     if [[ -z "${GITHUB_ACTIONS}" ]]; then
         docker buildx build --platform=linux/amd64 -t ${LOCAL_IMAGE_NAME} .
     else
-        docker build -t ${LOCAL_IMAGE_NAME} .
+        docker build -t ${LOCAL_IMAGE_NAME} -f .
     fi
 else
     echo "no need to build image ${LOCAL_IMAGE_NAME}"
 fi
+
+cd integration_test
 
 docker-compose up -d
 sleep 5
